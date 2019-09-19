7ee836fbdd320040be65a517494981db5ae3b09e
gmanriqe <gmanriqe@gmail.com>
2019-09-19 11:02:14 -0500

    Componente Registro de Usuario

--- sisadevmaster/src/app/app.module.ts
+++ sisadevmaster/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { RegisterUsuarioComponent } from './components/register-student/register-student.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    RegisterUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ sisadevmaster/src/app/components/register-student/register-student.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-usuario',
+  templateUrl: './register-student.component.html'
+})
+
+export class RegisterUsuarioComponent {
+  title = '';
+}
