4b8b1e011af351571d53a57f54abd9c1001a6d16
gmanriqe <gmanriqe@gmail.com>
2019-09-20 09:45:41 -0500

    Declarando la routa para el componente list-student

--- sisadevmaster/src/app/app.routes.ts
+++ sisadevmaster/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 // lo que importara dentro del layout base
 import { RegisterStudentComponent } from './components/register-student/register-student.component';
 import { RegisterTeacherComponent } from './components/register-teacher/register-teacher.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
 
 
 // para cualquier ruta (path: ''), me redireccionara al registroUsuario
 export const ROUTES: Routes = [
   { path: 'registroAlumno', component: RegisterStudentComponent },
   { path: 'registroDocente', component: RegisterTeacherComponent },
+  { path: 'listarAlumnos', component: ListStudentComponent },
   { path: '', pathMatch: 'full', redirectTo: 'registroAlumno' },
   { path: '**', pathMatch: 'full', redirectTo: 'registroAlumno' },
 ];
