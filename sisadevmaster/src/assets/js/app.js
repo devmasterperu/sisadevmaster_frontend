872fe52b426d8405a19da0a790ede1c6eb4f7f26
gmanriqe <gmanriqe@gmail.com>
2019-09-26 13:30:10 -0500

    Agregando JS de Bootstra y Maginific Popup, no CDN

--- sisadevmaster/src/assets/js/app.js
+++ sisadevmaster/src/assets/js/app.js
@@ -13,3 +13,11 @@ document.addEventListener('click', function(evnt) {
         evnt.preventDefault();
     }
 });
+
+jQuery(document).ready(function ($) {
+  $('.open-popup-link').magnificPopup({
+      type: 'inline',
+      midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+      closeBtnInside: true
+  });
+});
