621746be01fe9fbc29f48881d862e55d53f2e759
gmanriqe <gmanriqe@gmail.com>
2019-10-16 12:54:43 -0500

    Organizando codigo de App.module

--- sisadevmaster/src/app/app.module.ts
+++ sisadevmaster/src/app/app.module.ts
@@ -3,43 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module'; /* 👶🏼 */
 
-/*
-*  Componente principal
-*/
+/* Componente Principal */
 import { AppComponent } from './app.component'; /* 👶🏼 */
 
-/*
-*  Manejo rutas
-*/
+/* Importando rutas */
 import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
-/*
-*  Manejo de formularios
-*/
+/* Manejo de formularios */
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
-
-/*
-  Para que no me salga el error No provider for HttpClient! #20355 (No esta en el curso)
-  https://github.com/angular/angular/issues/20355
-*/
+/* Configuracion de HTTP */
 import { HttpClientModule } from '@angular/common/http';
 
-/*
-*  Componentes añadidos
-*/
-// import { HeaderComponent } from './components/header/header.component';
-// import { MenuComponent } from './components/menu/menu.component';
-/* Componentes Admin */
+/* Componentes Añadidos */
+// Componentes Admin
 import { InscriptionComponent } from './components/admin/inscription/inscription.component';
 import { PerfilAdminComponent } from './components/admin/perfill/perfil-admin.component';
-/* Componentes Student */
+// Componentes Student
 import { RegisterStudentComponent } from './components/student/register-student/register-student.component';
 import { EditStudentComponent } from './components/student/edit-student/edit-student.component';
 import { ListStudentComponent } from './components/student/list-student/list-student.component';
 import { PerfilComponent } from './components/student/perfil/perfil.component';
-/* Componentes Teacher */
+// Componentes Teacher
 import { RegisterTeacherComponent } from './components/teacher/register-teacher/register-teacher.component';
 import { ProgrammingCurseComponent } from './components/admin/programming-curse/programming-curse.component';
 import { PerfilTeacherComponent } from './components/teacher/perfil/perfil-teacher.component';
@@ -47,12 +34,10 @@ import { ConfigCurseComponent } from './components/teacher/config-curse/config-c
 import { ConfigQualificationComponent } from './components/teacher/config-qualification/config-qualification.component';
 import { ConfigAssistanceComponent } from './components/teacher/config-assistance/config-assistance.component';
 
-// importando rutas
-import { ROUTES } from './app.routes';
-
-// servicios
+/* Servicios */
 import { StudentService } from './services/student.service';
 import { TeacherService } from './services/teacher.service';
+import { ReusableComponent } from './components/reusable/reusable.component';
 
 // RouterModule.forRoot( ROUTES, { useHash: true } ) ✅ dentro de imports agregamos esa linea para el manejo de rutas
 // ⚠️ Eliminar en imports FormsModule, ya que estaremos trabajando con Reactive Forms Validation (Validación de formas reactivas) y no con ngForm
@@ -72,7 +57,8 @@ import { TeacherService } from './services/teacher.service';
     InscriptionComponent,
     PerfilAdminComponent,
     PerfilComponent,
-    EditStudentComponent
+    EditStudentComponent,
+    ReusableComponent
   ],
   imports: [
     BrowserModule,
